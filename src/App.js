6d394d9e87bf7f6bff52f20d116d141b162a0d37
ZamanMehmood <zaman342003@gmail.com>
2022-10-10 10:58:05 +0500

    Southville solution(Table Task)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Table from './components/table';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h2>Table designing</h2> */}
+      <Table />
+    </div>
+  );
+}
+
+export default App;
